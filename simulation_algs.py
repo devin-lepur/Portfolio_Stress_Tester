8ddddd879c9080dcd5f099b0fca24bfbe2fba909
Devin Lepur <73671652+devin-lepur@users.noreply.github.com>
2025-06-24 20:48:27 -0700

    Add files via upload
    Created file for simulation algorithms

---
+++ simulation_algs.py
@@ -0,0 +1,14 @@
+'''
+File: simulation_algs.py
+Purpose: Algorithms for simulating potential economic events
+Author: Devin Lepur
+Date: June 24, 2025
+'''
+
+# TODO: Value at Risk Estimator (VaR)
+
+
+# TODO: Historical crash simulation (Dot Com bubble, Housing Crisis, COVID-19)
+
+
+# TODO: Monte Carlo Simulation
