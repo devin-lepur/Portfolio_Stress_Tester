0c56ff29f2094a4cb8ae9dd292ce989e4b2f7174
Devin Lepur <73671652+devin-lepur@users.noreply.github.com>
2025-07-04 17:17:13 -0700

    Add files via upload
    Added descriptions and explanations for users

--- stress_tester.py
+++ stress_tester.py
@@ -18,7 +18,7 @@ import simulation_algs as sa
 st.title("Stock Portfolio Stress Tester")
 
 # Allow user to enter stock
-ticker_input = st.text_input("Enter the tickers of your holdings a list. Example: \"TSLA, AAPL, MSFT\"", key="ticker")
+ticker_input = st.text_input("Enter the tickers of your holdings a list. Example: \"SPY, AAPL, MSFT\"", key="ticker")
 
 # TODO: Allow user to upload downloaded holdings from brokerage
 
@@ -32,13 +32,14 @@ ticker_input = ticker_input.upper()
 if ticker_input.strip():
     tickers = [t.strip().upper() for t in ticker_input.split(",") if t.strip()]
     #Create data frame and allow editing
+
+    st.write('Use the "No. Shares held" column to manually enter how many shares you hold.')
     df = pd.DataFrame({"Tickers": tickers, "No. Shares held": [0.0]*len(tickers)})
     input_data = st.data_editor(df, num_rows="fixed", use_container_width=True)
 
 
     total_value = 0
-    st.markdown("## Estimated Current Value")
-    st.write("(Based on last market close)")
+    st.markdown("## Current Value of Your Portfolio")
 
 
 # TODO: Combine the two following for loops and just have the first loop pull from the selected data
@@ -63,12 +64,17 @@ if ticker_input.strip():
         except Exception as e:
             st.warning(f"Unable to fetch data for \"{ticker_symbol}\". Check to make sure you entered the tickers correctly.")
 
-    st.markdown(f"#### Total Portfolio Value: ${round(total_value, 2):,}")
+    st.markdown(f"#### Total Portfolio Value: ${round(total_value, 2):,}*")
+    st.write("*Based on last market close.")
+
+
 
 
 
     # Dynamic line graph of portfolio value over time
-    st.markdown("## Portfolio value over time")
+    st.markdown("## Portfolio Value Over Time")
+    st.write("*How has your portfolio performed historically?* Below is a representation of your portfolio " \
+    "performance throughout time. *Note this assumes you've held the same portfolio the entire time period.")
 
     # Drop down for time frame, 1 year default selection
     graph_time_period = st.selectbox("Select time frame", options = ["1mo", "6mo", "1y", "5y", "10y", "Max"], index=2)
@@ -99,6 +105,11 @@ if ticker_input.strip():
 
 
     try:
+        st.markdown("## Day-To-Day Performance")
+        st.write("*How does your portfolio perform each day?* Below we can see a breakdown " \
+        "of the percent return your portfolio provides. The height of each column indicates how often " \
+        "a return of that much occurs.")
+
         # Display histogram of daily returns
         percent_returns = dm.get_percent_returns(data=portfolio_timeline)["Percent_return"]
         
@@ -111,9 +122,11 @@ if ticker_input.strip():
         st.pyplot(fig)
 
 
-        # TODO: Allow user to select time frame for monte carlo display
-        # TODO: Only show Percentiles to bring down load times
-        # Monte Carlo Graph Presentation
+
+        # Monte Carlo Simulation
+        st.markdown("## Possible Future Performance")
+        st.write("*Where can we expect to see your portfolio in the future?* Below is a statisical " \
+        "representaion of where your portfolio could be given random returns on each day.")
 
         monte_carlo_period = st.selectbox("Select time frame for Monte Carlo Simulation", options = ["1mo", "6mo", "1y", "5y", "10y", "20y", "40y"], index=2)
     
@@ -129,7 +142,24 @@ if ticker_input.strip():
             columns=[f"{p}th Pctl" for p in percentiles_to_use]
         )
 
+        
         st.line_chart(monte_percentiles_df)
+
+        st.write("*How does the above simulation work?* The simulation used here is called a Monte Carlo " \
+        "Simulation. To do this simulation, we take historical daily returns of your portfolio and use them" \
+        " as possible returns on any given day. For every day of the time period you select, we randomly determine" \
+        " the return on that day and continue until we reach the period. This process is repeated 200 times and " \
+        "we select a few of the possible returns (here we choose these percentiles) and graph those.")
+
+        st.write("*How do I interpret this graph?* Each line represented is an important percentile of the " \
+        "data generated in the simulation. For example, the \"10th Pctl\" line means, \"10% of all the " \
+        "simulations ended below this line\". Additionally the \"75th Pctl\" line means 75% all simulations" \
+        " ended below this line.")
+
+        st.write("*Important note:* This simulation is based purely on random chance and is not representative" \
+        " of your actual portfolio over time. The simulation does not take into account various economic, " \
+        "political, or global conditions.")
+
     except:
         pass
 
